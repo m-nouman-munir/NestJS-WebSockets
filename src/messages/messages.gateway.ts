6a47e9c5170c5c6e04a76547494ec1c673dd022f
Muhammad Nouman Munir <noumanmunir458@gmail.com>
2024-07-17 17:05:12 +0500

    Sockets initialised with message sending and receiving events

---
+++ src/messages/messages.gateway.ts
@@ -0,0 +1,79 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketServer,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+import { MessagesService } from './messages.service';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({ cors: { origin: '*' } })
+export class MessagesGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private readonly messagesService: MessagesService) {}
+
+  @WebSocketServer()
+  private readonly _server: Server;
+
+  afterInit(server: Server) {
+    console.log('Websocket Messages Server Initialised!');
+    // console.log('Websocket Server : ' ,server);
+  }
+  handleConnection(client: any, ...args: any[]) {
+    console.log('Connected socket : ', client.id);
+    // console.log('Connected Client : ', client);
+  }
+  handleDisconnect(client: any) {
+    console.log('Disconnected socket : ', client.id);
+    // console.log('Disconnected Client : ', client);
+  }
+
+  @SubscribeMessage('sendMessage') // This is an Emitter Event same like GET or POST in REST API
+  create(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() createMessageDto: CreateMessageDto,
+  ) {
+    console.log('Sender socket : ', socket.id);
+
+    // return this.messagesService.create(createMessageDto); // Save the message to db or any other business logic
+    //After that
+
+    let recieverSocket: string = createMessageDto.receiverSocket; // Usually user socket is saved in a collection with user reference and message. Mostly this operation is performed in the handleConnection() method,where you check either the socket is available or needs to be saved in the database.
+
+    let messageBody = createMessageDto.message;
+
+    // This is called a Listener Event
+    let isMessageSentToReceiver: boolean = this._server
+      .to(recieverSocket)
+      .emit('message-received-event', messageBody);
+
+    return isMessageSentToReceiver; // or whatever
+  }
+
+  @SubscribeMessage('findAllMessages')
+  findAll() {
+    return this.messagesService.findAll();
+  }
+
+  @SubscribeMessage('findOneMessage')
+  findOne(@MessageBody() id: number) {
+    return this.messagesService.findOne(id);
+  }
+
+  @SubscribeMessage('updateMessage')
+  update(@MessageBody() updateMessageDto: UpdateMessageDto) {
+    return this.messagesService.update(updateMessageDto.id, updateMessageDto);
+  }
+
+  @SubscribeMessage('removeMessage')
+  remove(@MessageBody() id: number) {
+    return this.messagesService.remove(id);
+  }
+}
